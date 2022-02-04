ce5eb7a7d7583bda1bbe61219a85d7e4852184b3
RubisCO <54470624+RuBisCO28@users.noreply.github.com>
2022-02-04 22:54:39 +0900

    new_year_chaos

---
+++ hackerrank/new_year_chaos.py
@@ -0,0 +1,57 @@
+# time limit exceed solution
+# n = 8
+# 
+# def minimumBribes(q):
+#   ori = [i for i in range(1,n+1)]
+#   answer = 0
+#   for i in range(n):
+#     print(i, ori[i], q[i])
+#     print("before", ori)
+#     if ori[i] != q[i]:
+#       m = abs(ori.index(q[i])-i)
+#       answer += m
+#       if m <= 2:
+#         tmp = []
+#         tmp += ori[:i]
+#         tmp += [ori[i+m]]
+#         tmp += ori[i:i+m]
+#         if i+m != n - 1:
+#           tmp += ori[i+m+1:]
+#           print(ori[:i], ori[i+m], ori[i:i+m], ori[i+m+1:], m)
+#         else:
+#           print(ori[:i], ori[i+m], ori[i:i+m], m)
+#         ori = tmp
+#       else:
+#         return 'Too chaotic'
+#     print("after", ori)
+#   return answer
+# 
+# if __name__ == '__main__':
+#   q = [5,1,2,3,7,8,6,4]
+#   q = [1,2,5,3,7,8,6,4]
+#   print("goal", q)
+#   print("**********")
+#   result = minimumBribes(q)
+#   print(result)
+
+
+def minimumBribes(q):
+  bribes = 0
+  q = [i-1 for i in q]
+  for i, o in enumerate(q):
+    cur = i
+  
+    if o - cur > 2:
+      print("Too chaotic")
+      return
+    
+    for k in q[max(o - 1, 0):i]:
+      if k > o:
+        bribes += 1
+  
+  print(bribes)
+
+if __name__ == '__main__':
+  q = [5,1,2,3,7,8,6,4]
+  q = [1,2,5,3,7,8,6,4]
+  minimumBribes(q)
--- hackerrank/test.py
+++ hackerrank/test.py
@@ -1,79 +1,35 @@
-import copy
+n = 8
 
-r = 6
-c = 7
-
-def markBomb(og, ng):
-  for i in range(r):
-    for j in range(c):
-      if og[i][j] == 'O':
-        # center
-        ng[i][j] = '-'
-        # top
-        if i > 0:
-          ng[i-1][j] = '-'
-        # bottom
-        if i < r - 1:
-          ng[i+1][j] = '-'
-        # left
-        if j > 0:
-          ng[i][j-1] = '-'
-        # right
-        if j < c - 1:
-          ng[i][j+1] = '-'
-  return ng
-
-def plantBomb(ng):
-  for i in range(r):
-    for j in range(c):
-      if ng[i][j] == '.':
-        ng[i][j] = 'O'
-  return ng
-
-def detonateBomb(ng):
-  for i in range(r):
-    for j in range(c):
-      if ng[i][j] == '-':
-        ng[i][j] = '.'
-  return ng
-
-def bomberMan(n, grid):
-  og = []
-  ng = []
-  answer = []
-  for i in range(r):
-    og.append(list(grid[i]))
-    print(og[i])
-  for i in range(r):
-    ng.append(list(grid[i]))
-  print("-------------")
-  for i in range(1, n + 1):
-    if i % 3 == 1:
-      ng = markBomb(og, ng)
-      for i in range(r):
-        print(ng[i])
-      print("-------------")
-    elif i % 3 == 2:
-      ng = plantBomb(ng)
-      for i in range(r):
-        print(ng[i])
-      print("-------------")
-    elif i % 3 == 0:
-      ng = detonateBomb(ng)
-      og = []
-      for i in range(r):
-        og.append(copy.deepcopy(ng[i]))
-        print(og[i])
-      print("-------------")
-  for i in range(r):
-    ng[i] = ['.' if j== '-' else j for j in ng[i]]
-    answer.append(''.join(ng[i]))
-    print(answer[i])
+def minimumBribes(q):
+  ori = [i for i in range(1,n+1)]
+  answer = 0
+  for i in range(n):
+    print(i, ori[i], q[i])
+    print("before", ori)
+    if ori[i] != q[i]:
+      m = abs(ori.index(q[i])-i)
+      answer += m
+      if m <= 2:
+        tmp = []
+        tmp += ori[:i]
+        tmp += [ori[i+m]]
+        tmp += ori[i:i+m] 
+        if i+m != n - 1:
+          tmp += ori[i+m+1:]
+          print(ori[:i], ori[i+m], ori[i:i+m], ori[i+m+1:], m)
+        else:
+          print(ori[:i], ori[i+m], ori[i:i+m], m)
+        ori = tmp
+      else:
+        return 'Too chaotic'
+    print("after", ori)
   return answer
 
 if __name__ == '__main__':
-  n = 3
-  grid =['.......', '...O...', '....O..',\
-         '.......', 'OO.....', 'OO.....']
-  result = bomberMan(n, grid)
+  q = [5,1,2,3,7,8,6,4]
+  q = [1,2,5,3,7,8,6,4]
+  print("goal", q)
+  print("**********")
+  result = minimumBribes(q)
+  print(result)
 
