4fb4f0fa48356f75710867ba035da0fae52a5db7
RubisCO <54470624+RuBisCO28@users.noreply.github.com>
2022-02-12 19:12:12 +0900

    climbing_the_leaderboard

---
+++ hackerrank/sherlock_and_the_valid_string.py
@@ -0,0 +1,29 @@
+import collections
+
+def isValid(s):
+  c = collections.Counter(s)
+  print(c)
+  if len(set(c.values())) == 1:
+    return 'YES'
+  else:
+    max_v = max(c.values()) 
+    next_max_v = max_v - 1
+    po = 0
+    pt = 0
+    for i in c.values():
+      po += (i - next_max_v)
+      pt += (i - max_v)
+    if po == 1 or pt == (1 - max_v):
+      return 'YES'
+  return 'NO'
+
+if __name__ == '__main__':
+  s = 'abcc'
+  s = 'abcdefghhgfedecba'
+  s = 'abbccc'
+  s = 'aabbcd'
+  s = 'aabbc'
+  s = 'xxxaabbccrry'
+  s = 'ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd'
+  print(isValid(s))
+
