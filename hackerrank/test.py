7a29daa53ba0ad0ca05a0203ab072922f8f38ec0
RubisCO <54470624+RuBisCO28@users.noreply.github.com>
2022-03-07 22:05:00 +0900

    Chief Hopper

--- hackerrank/test.py
+++ hackerrank/test.py
@@ -1,14 +1,19 @@
-# 計算式はどちらもbe = 2 * be - arr[i]
-# 最終的にenergyが0になってればよい
-# 逆から計算して答えを導く
-# 割った余りはenergyに足す必要あり
-def chiefHopper(arr):
-  min_energy = 0
-  for i in range(len(arr)-1,-1,-1):
-    min_energy = (min_energy + arr[i]) // 2 + (min_energy + arr[i]) % 2
-  return min_energy
+def truckTour(petrolpumps):
+  start = 0
+  pav = 0
+  dfc = 0
+  for i in range(len(petrolpumps)):
+    pav += petrolpumps[i][0] - petrolpumps[i][1]
+    if pav < 0:
+      start = i + 1
+      dfc += pav
+      pav = 0
+  if pav+dfc >= 0:
+    return start
+  else:
+    return -1
 
 if __name__ == '__main__':
-  arr = [2,3,4,3,2]
-  arr = [3,4,3,2,4]
-  print(chiefHopper(arr))
+  petrolpumps = [[1,5],[10,3],[3,4]]
+  petrolpumps = [[6,4],[3,6],[7,3]]
+  print(truckTour(petrolpumps))
